21fc9497bda234112faf962ad7cdb16987e0e9d5
Khodor55 <157975803+Khodor55@users.noreply.github.com>
2025-05-04 12:22:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let header_nav = document.querySelector(' header .container nav');
+function openMenu(){
+    header_nav.classList.add('active')
+}
+function closeMenu(){
+    header_nav.classList.remove('active')
+}
